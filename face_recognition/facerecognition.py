300bd377f128fd6c12f09e3c5cb18b372f04645a
riftin2023 <noyal16072004@gmail.com>
2026-04-01 00:44:28 +0530

    defined the model and checked the metrics

--- face_recognition/facerecognition.py
+++ face_recognition/facerecognition.py
@@ -14,3 +14,29 @@ plt.style.use('dark_background')
 faces = fetch_lfw_people(min_faces_per_person = 60)
 print(faces.target_names)
 print(faces.images.shape)
+fig, ax = plt.subplots(3, 5)
+
+for i, axi in enumerate(ax.flat):
+    axi.imshow(faces.images[i], cmap='bone')
+    axi.set(xticks=[], yticks=[],
+            xlabel=faces.target_names[faces.target[i]])
+    pca = RandomizedPCA(n_components=150, whiten=True, random_state=42)
+svc = SVC(kernel='rbf', class_weight='balanced')
+
+model = make_pipeline(pca, svc)
+Xtrain, Xtest, ytrain, ytest = train_test_split(faces.data, faces.target, random_state=42)
+param_grid = {'svc__C': [1, 5, 10, 50], 'svc__gamma': [0.0001, 0.0005, 0.001, 0.005]}
+grid = GridSearchCV(model, param_grid)
+
+grid.fit(Xtrain, ytrain)
+print(grid.best_params_)
+model = grid.best_estimator_
+yfit = model.predict(Xtest)
+fig, ax = plt.subplots(4, 6)
+
+for i, axi in enumerate(ax.flat):
+    axi.imshow(Xtest[i].reshape(62, 47), cmap='bone')
+    axi.set(xticks=[], yticks=[])
+    axi.set_ylabel(faces.target_names[yfit[i]].split()[-1],
+                   color='black' if yfit[i] == ytest[i] else 'red')
+fig.suptitle('Predicted Names; Incorrect Labels in Red', size=14);
